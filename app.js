8a1d19a04c408ca8d9a61d43faeb36820cb59148
“scrymgeourggit config --global user.email alexcarrie.tidza@gmail.comgit commit --amend --reset-authorgit config --global user.name “scrymgeourg <alexcarrie.tidza@gmail.com>
2020-07-31 10:49:46 +0100

    handles 500 internal errors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api.router");
-const { send404 } = require("./controllers/errors.controllers");
+const {
+  send404,
+  handleInternalErrors,
+} = require("./controllers/errors.controllers");
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
 app.use(send404);
+
+app.use(handleInternalErrors);
 
 module.exports = app;
--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,3 +1,9 @@
 exports.send404 = (req, res, next) => {
   res.status(404).send({ message: "Resource not found." });
 };
+
+exports.handleInternalErrors = (err, req, res, next) => {
+  const { message } = err;
+
+  res.status(500).send({ message });
+};
